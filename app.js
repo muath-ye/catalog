a724654013f87bc1ee80eafe4552a9f2a18fa30b
muathye <muath.ye@gmail.com>
2023-07-15 09:54:51 +0300

    add url reader

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 const fs = require('fs');
+const https = require('https');
+const yargs = require('yargs');
 
-// Get the file path from the command line arguments
-const filePath = process.argv[2];
-
-// Read the file and print its contents to the console
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
+// Create a command line interface using yargs
+yargs.command({
+  command: 'cat <path>',
+  describe: 'Read the contents of a file or URL and print them to the console',
+  handler(argv) {
+    // Determine whether the path is a file path or a URL
+    if (argv.path.startsWith('http://') || argv.path.startsWith('https://')) {
+      // Read the URL and print its contents to the console
+      https.get(argv.path, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          console.log(data);
+        });
+      }).on('error', (err) => {
+        console.error(err);
+      });
+    } else {
+      // Read the file and print its contents to the console
+      fs.readFile(argv.path, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(data);
+      });
+    }
   }
-  console.log(data);
 });
+
+// Parse the command line arguments
+yargs.parse();
